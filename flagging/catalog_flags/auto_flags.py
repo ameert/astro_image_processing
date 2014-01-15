5b4028e9a8596c08b8e4570eeab6e9de2f06437b
Alan Meert <ameert@chitou.physics.upenn.edu>
2014-01-15 15:05:19 -0500

    added flagging programs for final catalog

---
+++ flagging/catalog_flags/auto_flags.py
@@ -0,0 +1,233 @@
+import numpy as np
+import pyfits as pf
+import pickle
+from mysql_class import *
+from flag_configuration import autoflag_vals, autoflag_dict, autoflag_config
+
+
+##############################################
+# BEGIN Flag test functions
+#############################################
+def check_inverted_bulge(data, light_cut):
+    result = []
+    for a,b in zip(data['bt_prof'], data['BT']):
+        result += [0]
+        sel_bt_r = np.extract(a[3]<light_cut, a[1])-0.5
+        transition = np.where(np.abs(sel_bt_r)< 0.01)
+        if b<.7:
+            if len(transition[0])>0:
+                last_trans = np.max(transition)
+                outer_val = np.sum(sel_bt_r[last_trans:])
+                if outer_val > 0:
+                    result[-1] = 1
+                
+    return np.array(result)
+
+def  parallel_com(data, rms_cut):
+    light_cut = 0.9
+    result = []
+    for a,b in zip(data['bt_prof'], data['BT']):
+        result += [0]
+        sel_bt_r = np.extract(a[3]<light_cut, a[1])
+        bt_m = sel_bt_r - np.mean(sel_bt_r)
+        bt_rms = np.sqrt(np.sum(bt_m*bt_m)/bt_m.size)
+        if bt_rms < rms_cut:
+            result[-1] = 1
+
+    return np.array(result)
+
+
+def check_bulge_dominates_always(data,light_cut):
+    result = []
+    for a in data['bt_prof']:
+        result += [0]
+        sel_bt_r = np.extract(a[3]<light_cut, a[1])-0.5
+        transition = np.where(sel_bt_r< 0.0,1,0)
+        if np.sum(transition)==0:
+            result[-1] = 1
+                
+    return np.array(result)
+
+def check_disk_dominates_always(data, light_cut):
+    result = []
+    for a in data['bt_prof']:
+        result += [0]
+        sel_bt_r = np.extract(a[3]<light_cut, a[1])-0.5
+        transition = np.where(sel_bt_r> 0.0,1,0)
+        if np.sum(transition)==0:
+            result[-1] = 1
+                
+    return np.array(result)
+
+def check_inverted_disk(data, rad_cut):
+    result = []
+    for c, a,b in zip(data['galcount'],data['bt_prof'], data['BT']):
+        result += [0]
+        if b< 0.75:
+            sel_bt_r = np.extract(a[0]<rad_cut, a[1])-0.5
+            transition = np.where(np.where(sel_bt_r[:-1]< 0,1,0)*np.where(sel_bt_r[1:]> 0,1,0)==1)[0]+1
+            trans_to_disk =np.where(np.where(sel_bt_r[:-1]> 0,1,0)*np.where(sel_bt_r[1:]< 0,1,0)==1)[0]+1
+            if len(transition)>0:
+                first_trans = np.min(transition)
+                if len(trans_to_disk) > 0:
+                    to_disk_min = np.min(trans_to_disk)
+                else:
+                    to_disk_min = 999.0
+                if first_trans< to_disk_min:
+                    inner_val = np.sum(sel_bt_r[:first_trans])
+                    if inner_val < 0:
+                        result[-1] = 1
+
+    return np.array(result)
+
+##############################################
+# END Flag test functions
+#############################################
+
+def run_auto_flags(model, band, folder_num, print_flags=False):
+    """Runs the autoflags and produces autoflag picklefile that can be loaded
+to a mysql database"""
+    
+    infile = open('/home/ameert/to_classify/flagfiles/%s/%s/total_flag_%d.pickle' %(band,model,folder_num))
+    data = pickle.load(infile)
+    infile.close()
+
+    infile = open('/home/ameert/to_classify/flagfiles/%s/%s/total_profile_%d.pickle' %(band,model,folder_num))
+    profile_data = pickle.load(infile)
+    infile.close()
+
+    profile_dict = dict(zip(profile_data['galcount'], zip(profile_data['chi_prof'], profile_data['bt_prof'], profile_data['im_ctr'])))
+
+    data['im_ctr'] = [ profile_dict.get(a, (([-999.0],[-999.0]),([-999.0], [-999.0], [-999.0],[-999.0]), (-999.0, -999.0)))[2] for a in data['galcount']]
+    data['chi_prof'] = [ profile_dict.get(a, (([-999.0],[-999.0]),([-999.0], [-999.0], [-999.0],[-999.0]), (-999.0, -999.0)))[0] for a in data['galcount']]
+    data['bt_prof'] = [ profile_dict.get(a, (([-999.0],[-999.0]),([-999.0], [-999.0], [-999.0],[-999.0]), (-999.0, -999.0)))[1] for a in data['galcount']]
+
+    auto_flags = np.zeros_like(np.array(data['galcount'])).astype(int)
+
+# various metrics used to calculate flags
+    fit_center_x = data['x_bulge']* data['BT']+ data['x_disk']*(1.0- data['BT'])
+    fit_center_y = data['y_bulge']* data['BT']+ data['y_disk']*(1.0- data['BT'])
+
+    imcenter_x = np.array([a[0] for a in data['im_ctr']])
+    imcenter_y = np.array([a[1] for a in data['im_ctr']])
+
+    separation = np.sqrt((fit_center_x-imcenter_x)**2+(fit_center_y-imcenter_y)**2)
+    separation = separation*0.396/data['r_sex']
+
+    new_chi = np.array([a[1][19] if len(a[1])>21 else -999 for a in data['chi_prof'] ])
+    new_der = np.array([np.sum(a[1][1:19]-a[1][0:18]) if len(a[1])>21 else -999 for a in data['chi_prof'] ])
+
+    ang1 = np.radians(data['pa_bulge'])
+    ang2 = np.radians(data['pa_disk'])
+    pa_diff = np.arccos(np.cos(ang1)*np.cos(ang2)+np.sin(ang1)*np.sin(ang2))
+    pa_diff = np.abs(np.degrees(pa_diff))
+    pa_diff = np.where(pa_diff>90.0, 180.0-pa_diff, pa_diff)
+
+    cir_rad_bulge =  data['r_bulge']*np.sqrt(data['ba_bulge'])
+    cir_rad_disk =  data['r_disk']*np.sqrt(data['ba_disk'])
+
+    bulge_vs_sky = cir_rad_bulge/data['r_sex']
+    disk_vs_sky = cir_rad_disk/data['r_sex']
+    rad_rat = cir_rad_bulge/(1.678*cir_rad_disk)
+
+    # set flags
+    #high_chi = np.where(np.where(new_chi>autoflag_config['chicut'], 1,0)+np.where(new_der+new_chi>7,1,0)*np.where(new_chi>5,1,0)>0, 1,0)
+    high_chi = np.where(new_chi>autoflag_config['chicut'], 1,0)
+    center_probs = np.where(separation>autoflag_config['max_sep'], 1,0)
+    posang_diff = np.where(data['ba_disk']<autoflag_config['posang_d']['ba_cut'],1,0) * \
+                 np.where(data['ba_bulge']<autoflag_config['posang_d']['ba_cut'],1,0) * \
+                 np.where(pa_diff>autoflag_config['posang_d']['pa_cut'], 1,0)    
+
+    no_bulge = np.where(autoflag_config['bulge_cut'](data['BT'], data['m_bulge']), 1,0)
+    no_disk = np.where(autoflag_config['disk_cut'](data['BT'], data['m_disk']), 1,0)  
+
+    parallel_com_flag = parallel_com(data, autoflag_config['par_com'])*np.where(no_bulge==1, 0,1)*np.where(no_disk==1, 0,1)*np.where(data['n_bulge']<autoflag_config['disky_n'],1,0)
+
+    posang_disk = posang_diff*np.where(data['BT']>=0.5, 1,0)*np.where(no_disk==1, 0,1)
+
+    disk_fitting_inner = check_inverted_disk(data,autoflag_config['disk_rad_cut'])*np.where(no_disk==1, 0,1)*np.where(no_bulge==1, 0,1)
+
+    disk_is_sky = np.where(disk_vs_sky>autoflag_config['disk_sky_cut'], 1,0)* np.where(data['ba_disk']>autoflag_config['disk_sky_ba'], 1,0)*np.where(no_disk==1, 0,1)
+    disk_contamination = np.where(disk_vs_sky>autoflag_config['disk_sky_cut'], 1,0)* np.where(data['ba_disk']<=autoflag_config['disk_sky_ba'], 1,0)*np.where(no_disk==1, 0,1)
+    high_e_disk = np.where(data['ba_disk']<autoflag_config['disk_ba_cut'],1,0)*np.where(data['BT']>autoflag_config['disk_ba_BT'],1,0)*np.where(no_disk==1, 0,1)
+    disk_dominates_always = check_disk_dominates_always(data, autoflag_config['disk_dom_light'])*np.where(no_bulge==1, 0,1)
+
+    posang_bulge = posang_diff*np.where(data['BT']<0.5, 1,0)*np.where(no_bulge==1, 0,1)        
+    bulge_fitting_outer = check_inverted_bulge(data, autoflag_config['bulge_light_cut'])*np.where(no_bulge==1, 0,1)
+    bulge_is_sky = np.where(bulge_vs_sky>autoflag_config['bulge_sky_cut'], 1,0)* np.where(data['ba_bulge']>autoflag_config['bulge_sky_ba'], 1,0)*np.where(no_bulge==1, 0,1)
+    bulge_contamination = np.where(bulge_vs_sky>autoflag_config['bulge_sky_cut'], 1,0)* np.where(data['ba_bulge']<=autoflag_config['bulge_sky_ba'], 1,0)*np.where(no_bulge==1, 0,1)
+    high_e_bulge = np.where(data['ba_bulge']<autoflag_config['bulge_ba_cut'],1,0)*np.where(data['BT']<autoflag_config['bulge_ba_BT'],1,0)*np.where(no_bulge==1, 0,1)
+    bulge_is_disk_flag = np.where(rad_rat>autoflag_config['bulge_disk_rat'],1,0)*np.where(data['n_bulge']<autoflag_config['disky_n'],1,0)*np.where(no_bulge==1, 0,1)
+    bulge_low_n_flag = np.where(bulge_is_disk_flag,0,1)*np.where(data['n_bulge']<autoflag_config['disky_n'],1,0)*np.where(no_bulge==1, 0,1)
+    bulge_dominates_always = check_bulge_dominates_always(data,autoflag_config['bulge_dom_light'])*np.where(no_disk==1, 0,1)
+
+    flip_com_flag = np.where(bulge_is_disk_flag+bulge_fitting_outer+disk_fitting_inner >1, 1,0)| np.where(disk_fitting_inner+bulge_low_n_flag >1, 1,0)|np.where(bulge_is_disk_flag >=1, 1,0)|np.where(no_disk+bulge_low_n_flag >1, 1,0)
+
+    auto_disk_total = np.where(disk_is_sky+disk_contamination+ disk_fitting_inner*np.where(bulge_is_disk_flag+bulge_fitting_outer+bulge_low_n_flag+ parallel_com_flag>0,0,1)+high_e_disk*posang_disk+parallel_com_flag>0,1,0)
+    auto_bulge_total = np.where(bulge_fitting_outer*np.where(disk_fitting_inner+bulge_is_disk_flag>0,0,1)+bulge_is_sky+bulge_contamination+ high_e_bulge*posang_bulge+parallel_com_flag>0,1,0)
+    bad_total_flag = np.where((np.where(disk_is_sky+disk_contamination+bulge_is_sky+bulge_contamination+center_probs>0, 1,0) + np.where(no_bulge+no_disk+disk_dominates_always+bulge_dominates_always>1,1,0))>0,1,0)+high_chi
+
+    # if galfit failed, or some measurement is wrong in the pa
+    galfit_fail = np.where(data['galfit_flag']==1,1,0) #| np.where((bulge_dominates_always+disk_dominates_always+no_bulge+no_disk)>1, 1,0)
+
+    # only log disk flags if disk is included
+    if model in ['devexp', 'serexp']:
+        auto_flags += posang_disk * 2**autoflag_dict['disk pa problem']
+        auto_flags += disk_fitting_inner * 2**autoflag_dict['disk fitting inner']
+        auto_flags += disk_is_sky * 2**autoflag_dict['disk is sky']
+        auto_flags += disk_contamination * 2**autoflag_dict['disk contaminated']
+        auto_flags += high_e_disk * 2**autoflag_dict['high e disk']
+        auto_flags += auto_disk_total * 2**autoflag_dict['bad disk']
+        auto_flags += disk_dominates_always * 2**autoflag_dict['disk dominates always']
+        auto_flags += parallel_com_flag * 2**autoflag_dict['parallel components']
+        auto_flags += flip_com_flag * 2**autoflag_dict['flip components']
+
+    auto_flags += no_disk * 2**autoflag_dict['no disk likely']
+    auto_flags += bulge_is_disk_flag * 2**autoflag_dict['bulge is disk']
+    auto_flags += posang_bulge * 2**autoflag_dict['bulge pa problem']
+    auto_flags += bulge_fitting_outer * 2**autoflag_dict['bulge fitting outer']
+    auto_flags += bulge_is_sky * 2**autoflag_dict['bulge is sky']
+    auto_flags += bulge_contamination * 2**autoflag_dict['bulge contaminated']
+    auto_flags += no_bulge * 2**autoflag_dict['no bulge likely']
+    auto_flags += high_e_bulge * 2**autoflag_dict['high e bulge']
+    auto_flags += bulge_low_n_flag * 2**autoflag_dict['low n bulge']
+    auto_flags += bulge_dominates_always * 2**autoflag_dict['bulge dominates always']
+
+    auto_flags +=  auto_bulge_total * 2**autoflag_dict['bad bulge']
+
+    auto_flags += center_probs * 2**autoflag_dict['centering']
+    auto_flags += high_chi * 2**autoflag_dict['high chi^2']
+    auto_flags += bad_total_flag * 2**autoflag_dict['Bad total fit']
+
+    # if galfit failed, lets clear out all the flags...They are junk!
+    auto_flags = np.where(galfit_fail==1, 0, auto_flags)
+    auto_flags += galfit_fail * 2**autoflag_dict['galfit failure']
+
+    outfile = open('/home/ameert/to_classify/flagfiles/%s/%s/autoflags_%d_revised.pickle' %(band, model,folder_num), 'w')
+    pickle.dump({'galcount':data['galcount'],'autoflags':auto_flags},outfile)
+    outfile.close()
+    if print_flags:
+        print "autoflags"
+        for a in zip(data['galcount'], auto_flags):
+            print a[0], a[1]
+    return
+
+
+def load_autoflag(folder_num, info_dict, print_info = False):
+    cursor = info_dict['cursor']
+
+    if print_info:
+        print 'loading folder %d' %folder_num
+        print '/home/ameert/to_classify/flagfiles/%s/%s/autoflags_%d_revised.pickle' %(info_dict['band'],info_dict['model'],folder_num)
+    infile = open('/home/ameert/to_classify/flagfiles/%s/%s/autoflags_%d_revised.pickle' %(info_dict['band'],info_dict['model'],folder_num))
+    data = pickle.load(infile)
+    infile.close()
+
+    for galcount,flagval in zip(data['galcount'], data['autoflags']):
+        cmd = """update {table} set flag = {finalval} where galcount = {galcount} and band = '{band}' and model = '{model}' and ftype = '{autoflag_ftype}';""".format(table = 'Flags_optimize', finalval = flagval, band=info_dict['band'], model=info_dict['model'], galcount = galcount, autoflag_ftype=info_dict['autoflag_ftype'])
+        if print_info:
+            print cmd
+        cursor.execute(cmd)
+
+    return
+
---
+++ flagging/catalog_flags/u_flags.py
@@ -0,0 +1,163 @@
+from mysql_class import *
+from flag_configuration import autoflag_dict, uflag_dict
+from flag_analysis import flag_set, get_percent
+
+def run_uflags(folder_num, info_dict, print_flags = False):
+    cursor = info_dict['cursor']
+
+    cmd = """select a.galcount, a.flag, z.BT from {table} as a, M2010 as b, r_band_{model} as z where a.flag >=0 and a.band = '{band}' and a.model = '{model}' and a.ftype = '{autoflag_ftype}' and a.galcount = b.galcount and a.galcount = z.galcount and (a.galcount between {low_gal} and {high_gal}) order by a.galcount;""".format(table = 'Flags_optimize', band=info_dict['band'], model=info_dict['model'], autoflag_ftype=info_dict['autoflag_ftype'], low_gal = 250*(folder_num-1)+1, high_gal = 250*folder_num)
+    
+    galcount, flags, BT = cursor.get_data(cmd)
+    galcount = np.array(galcount, dtype = int)
+    flags = np.array(flags, dtype = int)
+    BT = np.array(BT, dtype =float)
+
+
+    bulge_dominates = flag_set(np.where(BT>=0.5,1,0))
+
+    galfit_failure = flag_set(np.where(flags&2**autoflag_dict['galfit failure']>0,1,0))
+    center_probs = flag_set(np.where(flags&2**autoflag_dict['centering']>0,1,0))
+    bulge_contam_or_sky = flag_set(
+              np.where(flags&2**autoflag_dict['bulge contaminated']>0,1,0)
+              | np.where(flags&2**autoflag_dict['bulge is sky']>0,1,0))
+    disk_contam_or_sky = flag_set(
+              np.where(flags&2**autoflag_dict['disk contaminated']>0,1,0)
+              | np.where(flags&2**autoflag_dict['disk is sky']>0,1,0))
+
+
+    disk_inner = flag_set(np.where(flags&2**autoflag_dict['disk fitting inner']>0,1,0))
+    bulge_outer = flag_set(np.where(flags&2**autoflag_dict['bulge fitting outer']>0,1,0))
+    bulge_is_disk = flag_set(np.where(flags&2**autoflag_dict['bulge is disk']>0,1,0))
+    low_n_bulge = flag_set(np.where(flags&2**autoflag_dict['low n bulge']>0,1,0))
+    
+    # select out the 1 component cases
+    no_bulge = flag_set(np.where(flags&2**autoflag_dict['no bulge likely']>0,1,0))
+    no_disk = flag_set(np.where(flags&2**autoflag_dict['no disk likely']>0,1,0))
+    disk_dominates_always = flag_set(np.where(flags&2**autoflag_dict['disk dominates always']>0,1,0))
+    bulge_dominates_always = flag_set(np.where(flags&2**autoflag_dict['bulge dominates always']>0,1,0))
+
+    other_bad_fits = flag_set(np.where(no_bulge.vals+no_disk.vals+
+                     disk_dominates_always.vals+
+                     bulge_dominates_always.vals > 1, 1,0)|(bulge_dominates.vals*(no_bulge.vals|disk_dominates_always.vals))|(bulge_dominates.invert()*(no_disk.vals|bulge_dominates_always.vals)))
+             # these are fits that have incorrect measurements ...
+  
+    bad_gals = flag_set( galfit_failure.vals|center_probs.vals |bulge_contam_or_sky.vals |disk_contam_or_sky.vals| other_bad_fits.vals)
+
+    #now update the 1com gals to remove bad ones
+    no_bulge.vals = no_bulge.vals*bad_gals.invert()
+    no_disk.vals = no_disk.vals*bad_gals.invert()
+    disk_dominates_always.vals = disk_dominates_always.vals*bad_gals.invert()
+    bulge_dominates_always.vals = bulge_dominates_always.vals*bad_gals.invert()
+
+    parallel_com = flag_set(bad_gals.invert()*np.where(flags&2**autoflag_dict['parallel components']>0,1,0))
+
+    #now update again to remove parallel components
+    no_bulge.vals = no_bulge.vals*parallel_com.invert()
+    no_disk.vals = no_disk.vals*parallel_com.invert()
+    disk_dominates_always.vals = disk_dominates_always.vals*parallel_com.invert()
+    bulge_dominates_always.vals = bulge_dominates_always.vals*parallel_com.invert()
+
+    ser_bulge_dominates_always = flag_set(bulge_dominates_always.vals*bulge_is_disk.invert()*low_n_bulge.invert())
+    flip_bulge_dominates_always = flag_set(bulge_dominates_always.vals*(bulge_is_disk.vals|low_n_bulge.vals))
+
+    ser_no_disk = flag_set(no_disk.vals*bulge_is_disk.invert()*low_n_bulge.invert())
+    flip_no_disk = flag_set(no_disk.vals*(bulge_is_disk.vals|low_n_bulge.vals))
+
+    single_bulge = flag_set(ser_no_disk.vals|ser_bulge_dominates_always.vals)
+    single_disk = flag_set(flip_no_disk.vals|flip_bulge_dominates_always.vals
+                           |no_bulge.vals | disk_dominates_always.vals|
+                           parallel_com.vals )
+
+    prob_2com_gals = flag_set(bad_gals.invert()*parallel_com.invert()*no_bulge.invert()*no_disk.invert() *disk_dominates_always.invert() *bulge_dominates_always.invert())
+    no_flags = flag_set(np.where(flags==0,1,0))
+
+    bulge_truly_outer = flag_set(bulge_outer.vals*bulge_is_disk.invert()*disk_inner.invert()*low_n_bulge.invert()*prob_2com_gals.vals)
+    disk_truly_inner = flag_set(bulge_outer.invert()*bulge_is_disk.invert()*disk_inner.vals*prob_2com_gals.vals)
+
+    bulge_shape = flag_set(np.where(flags&2**autoflag_dict['high e bulge']>0,1,0)* np.where(flags&2**autoflag_dict['bulge pa problem']>0,1,0))
+    disk_shape = flag_set(np.where(flags&2**autoflag_dict['high e disk']>0,1,0)* np.where(flags&2**autoflag_dict['disk pa problem']>0,1,0))
+
+    bad_bulge_shapes = flag_set(prob_2com_gals.vals*bulge_dominates.vals*disk_shape.vals*bulge_truly_outer.invert()*disk_truly_inner.invert())
+    bad_disk_shapes = flag_set(prob_2com_gals.vals*bulge_dominates.invert()*bulge_shape.vals*bulge_truly_outer.invert()*disk_truly_inner.invert())
+
+    problem_2com_fits =flag_set(prob_2com_gals.vals*( bulge_truly_outer.vals|disk_truly_inner.vals | bad_bulge_shapes.vals | bad_disk_shapes.vals))
+
+    good_flip_2com =  flag_set( prob_2com_gals.vals*problem_2com_fits.invert()* no_flags.invert()*(np.where(bulge_is_disk.vals+bulge_outer.vals+disk_inner.vals >1, 1,0)| disk_inner.vals*low_n_bulge.vals | bulge_is_disk.vals))
+    good_good_2com =  flag_set(prob_2com_gals.vals* problem_2com_fits.invert()*no_flags.invert()*good_flip_2com.invert())
+
+
+    cats_to_plot = [("Good Total Magnitudes and Sizes", bad_gals.invert()),
+                    ("\tTwo-Component Galaxies", prob_2com_gals.vals*problem_2com_fits.invert()), 
+                    ("\t\tNo Flags", no_flags.vals),   
+                    ("\t\tGood Ser, Good Exp (Some Flags)",good_good_2com.vals ), 
+                    ("\t\tFlip Components, n$_{Ser}<$2", good_flip_2com.vals),     
+                    ("\tBulge Galaxies", single_bulge.vals),
+                    ("\t\tNo Exp Component, n$_{Ser}>=$2", ser_no_disk.vals),
+                    ( "\t\tSer Dominates Always, n$_{Ser}>=$2", ser_bulge_dominates_always.vals),
+
+                    ("\tDisk Galaxies", single_disk.vals),
+                    ("\t\tNo Ser Component", no_bulge.vals),
+                    ( "\t\tNo Exp, n$_{Ser}<$2, Flip Components", flip_no_disk.vals),
+                    ( "\t\tSer Dominates Always, n$_{Ser}<$2",flip_bulge_dominates_always.vals),
+                    ("\t\tExp Dominates Always", disk_dominates_always.vals),
+                    ( "\t\tParallel Components", parallel_com.vals),
+                 
+                    ("\tProblemmatic Two-Component Galaxies", problem_2com_fits.vals),
+                    ("\t\tSer Outer Only", bulge_truly_outer.vals),
+                    ("\t\tExp Inner Only", disk_truly_inner.vals),
+                    ("\t\tGood Ser, Bad Exp, B/T$>=$0.5", bad_bulge_shapes.vals),
+                    ("\t\tBad Ser, Good Exp, B/T$<$0.5", bad_disk_shapes.vals),
+
+                    ("Bad Total Magnitudes and Sizes", bad_gals.vals),
+                    ("\tCentering Problems", center_probs.vals),  
+                    ("\tSer Component Contamination by Neighbors or Sky", bulge_contam_or_sky.vals), 
+                    ("\tExp Component Contamination by Neighbors or Sky", disk_contam_or_sky.vals),
+                    ("\tBad Ser and Bad Exp Components", other_bad_fits.vals),    
+                    ("\tGalfit Failure", galfit_failure.vals),   
+                    ]
+    
+    uflags = np.zeros_like(flags)
+ 
+    for a in cats_to_plot:
+        if a[0] in uflag_dict.keys():
+            uflags += a[1]*(2**uflag_dict[a[0]])
+        else:
+            print "Not setting flag '%s' ... It doesn't exist!!" %a[0]
+
+    if print_flags:
+        print "uflags"
+        for a in zip(galcount, uflags):
+            print a[0], a[1]
+
+    
+    return galcount,uflags
+
+def load_uflags(galcount, uflags, info_dict, print_info = False):
+    cursor = info_dict['cursor']
+
+    for gal, flagval in zip(galcount, uflags):
+        cmd = """update {table} set flag = {finalval} where galcount = {galcount} and band = '{band}' and model = '{model}' and ftype = '{uflag_ftype}';""".format(table = 'Flags_optimize', finalval = flagval, band=info_dict['band'], model=info_dict['model'], galcount = gal, uflag_ftype=info_dict['uflag_ftype'])
+        if print_info:
+            print cmd
+        cursor.execute(cmd)
+    return
+
+
+if __name__ == "__main__":
+    
+    info_dict = {'dba':'catalog', 'usr':'pymorph', 'pwd':'pymorph', 'host':'',
+                 'band':'r', 'model':'serexp','autoflag_ftype':'r',
+                 'uflag_ftype':'u',}
+    info_dict['cursor']=mysql_connect(info_dict['dba'],info_dict['usr'],info_dict['pwd'],info_dict['host'])
+    
+    folder_num = int(sys.argv[1])
+    print "\n\nFor the %s band %s Catalog" %(info_dict['band'],info_dict['model'])
+    galcount, uflags_out = run_uflags(folder_num, info_dict,print_flags = False)
+    load_uflags(galcount, uflags_out, info_dict, print_info = False)
+    
+
+
+
+
+
+
