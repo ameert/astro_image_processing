024ebfd15278f176ab32ab4dc47e76cde2647ffa
Alan Meert <alan.meert@gmail.com>
2014-03-07 17:45:26 -0500

    minor changes to files

--- make_final_catalog/simulation_tables.py
+++ make_final_catalog/simulation_tables.py
@@ -28,3 +28,23 @@ alter table M2010 change column galcount true_galcount int;
 alter table M2010 drop primary key;
 alter table M2010 add column galcount int primary key first;
 insert into M2010 select a.simcount, b.*  from sim_input as a, catalog.M2010 as b where a.galcount = b.galcount;
+
+update CAST as a, catalog.CAST as c set a.gain_r = c.gain_r, a.gain_g = c.gain_g, a.gain_i = c.gain_i where a.true_galcount = c.galcount;
+update CAST as a, CAST as c set a.gain_r = c.gain_r, a.gain_g = c.gain_g, a.gain_i = c.gain_i  where a.galcount >61 and a.cluster_num=c.cluster_num;
+
+update CAST as a, catalog.CAST as c set a.run = c.run, a.rerun = c.rerun, 
+a.camcol = c.camcol,a.field = c.field where a.true_galcount = c.galcount;
+update CAST as a, CAST as c set a.run = c.run, a.rerun = c.rerun, 
+a.camcol = c.camcol,a.field = c.field  where a.galcount >61 and a.cluster_num=c.cluster_num;
+
+update CAST as a, z_sims_g as b set a.aa_g = -1.0*b.zeropoint where a.galcount = b.simcount;
+update CAST as a, z_sims_g as b set a.z = b.z where a.galcount = b.simcount;
+update CAST as a, z_sims_r as b set a.aa_r = -1.0*b.zeropoint where a.galcount = b.simcount;
+
+update DERT as a,  z_sims_r as b set a.kcorr_r = b.kcorr where a.galcount = b.simcount;
+update DERT as a, z_sims_g as b set a.kcorr_g = b.kcorr where a.galcount = b.simcount;
+update DERT as a,  z_sims_g as b set a.dismod = b.dismod where a.galcount = b.simcount;
+update DERT as a, z_sims_g as b set a.kpc_per_arcsec = b.kpc_per_arcsec where a.galcount = b.simcount;
+
+update CAST as a, CAST as c set a.run = c.run, a.rerun = c.rerun, 
+a.camcol = c.camcol,a.field = c.field  where a.galcount >61 and a.cluster_num=c.cluster_num;
