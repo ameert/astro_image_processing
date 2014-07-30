07e7b6652bce22ccef15843319abb38b8cfcfcef
Alan Meert <alan.meert@gmail.com>
2014-07-30 14:47:22 -0400

    more changes to query code

--- casjobs-querey/query_config.py
+++ casjobs-querey/query_config.py
@@ -3,9 +3,9 @@ gal_cat = {'filename':'spectro_sample_raw.cat',
            'out_file':'spectro_sample.cat'
            }
 
-casjobs_info = { 'username':'upenn_pymorph',
-                 'wsid':'396840617',
-                 'password':'pymorph_upenn',
-                 'cas_jar_path':'./casjobs.jar',
+casjobs_info = { 'username':'',
+                 'wsid':'',
+                 'password':'',
+                 'cas_jar_path':'casjobs.jar',
                  'jobname':'test_name',
-                 'search_target':'DR7'}
+                 'search_target':'DR8'}
