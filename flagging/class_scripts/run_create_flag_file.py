075e3b0ff845251555c24c41b4dcfe7d53d86530
Alan Meert <alan.meert@gmail.com>
2014-09-23 14:11:45 -0400

    cleaned up old flagging routines that are no longer used

--- flagging/class_scripts/run_create_flag_file.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for band in 'gri':
-    for model in ['dev','ser','devexp','serexp']:
-        for count in range(1,2684):
-            print "file number: %d" %count
-            os.system('python create_flag_file_catalog.py %d %s %s' %(count, model, band))
-
-
