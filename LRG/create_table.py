6b984ebc0df65e1bf6de26ad826ce4a0d6b078d0
Alan Meert <ameert@sas.upenn.edu>
2014-11-23 14:18:31 -0500

    More updates to the LRG scripts

--- LRG/create_table.py
+++ LRG/create_table.py
@@ -1,11 +1,12 @@
 from astro_image_processing import mysql
+from astro_image_processing.user_settings import *
 import pyfits as pf
 import numpy as np
 from scipy import integrate
 import sys
 
 dir = '/home/clampitt/filaments/spatial_cats/'
-patches = np.arange(1, 2, 1)
+patches = np.arange(0, 33, 1)
 tablename="LRG_pairs";
 
 #connect to the database
@@ -15,7 +16,7 @@ cursor = mysql.mysql_connect('LRGPairs',mysql_params['user'],
 
 #prepare a new table
 cursor.execute("DROP TABLE IF EXISTS %s;" %tablename)
-cursor.execute("CREATE TABLE %s (id int primary key auto_increment, ra_mid float default -999, dec_mid float default -999, z float default -999, id1 int default -999, ra1 float default -999, dec1 float default -999, id2 int default -999, ra2 float default -999, dec2 float default -999, R_pair float default -999, drlos float default -999, dz float default -999, patch int defualt -999, dismod float -999, kpc_per_arcsec float -999);" %tablename)
+cursor.execute("CREATE TABLE %s (id int primary key auto_increment, ra_mid float default -999, dec_mid float default -999, z float default -999, id1 int default -999, ra1 float default -999, dec1 float default -999, id2 int default -999, ra2 float default -999, dec2 float default -999, R_pair float default -999, drlos float default -999, dz float default -999, patch int default -999, dismod float default -999, kpc_per_arcsec float default -999);" %tablename)
 
 #commit the changes
 cursor.Conn.commit()
@@ -27,11 +28,11 @@ for patch in patches:
     lensfile = 'pair-cat-nov4_LRG_Rmax24.0_rlos6.0_p%d.fit' % (patch)
     hdu = pf.open(dir + lensfile)
     data = hdu[1].data
-    print hdu[1].header
     for i in data:
-        cmd+= '(%f, %f, %f, %d, %f, %f, %d, %f, %f, %f, %f, %f, %d),'  %(*i, patch)
+        i = list(i)
+        i.append(patch)
+        cursor.execute(cmd +' (%f, %f, %f, %d, %f, %f, %d, %f, %f, %f, %f, %f, %d);' %tuple(i))
+    
     hdu.close()
-    cmd[-1]=';'
-    cursor.execute(cmd)
     cursor.Conn.commit()
 
