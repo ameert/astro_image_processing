e122b3e342ec955bdf10091a5d951bbca3a1b0c3
Alan Meert <ameert@sas.upenn.edu>
2014-11-23 17:23:25 -0500

    final changes to load script

--- LRG/create_table.py
+++ LRG/create_table.py
@@ -7,7 +7,7 @@ import sys
 from cosmocal import *
 
 dir = '/home/clampitt/filaments/spatial_cats/'
-patches = np.arange(0, 33, 1)
+patches = np.arange(0, 34, 1)
 tablename="LRG_pairs"
 H0 = 100.0
 WM = 0.3
@@ -30,21 +30,24 @@ cursor.Conn.commit()
 #now load the data one patch at a time
 for patch in patches:
     print('Patch number %d' % (patch))
-    cmd = "insert into %s (ra_mid, dec_mid, z, id1, ra1, dec1, id2, ra2, dec2, R_pair, drlos, dz, patch, dismod, kpc_per_arcsec) values " %tablename
     lensfile = 'pair-cat-nov4_LRG_Rmax24.0_rlos6.0_p%d.fit' % (patch)
     hdu = pf.open(dir + lensfile)
     data = hdu[1].data
+    cmd = "insert into %s (ra_mid, dec_mid, z, id1, ra1, dec1, id2, ra2, dec2, R_pair, drlos, dz, patch, dismod, kpc_per_arcsec) values " %tablename
+    count=0
     for i in data:
-        i = list(i)
-        i.append(patch)
         try:
             dismod, kpc_per_arcsec = cal(i[2], H0, WM, WV, pixelscale)[2:4]    
         except:
             dismod, kpc_per_arcsec = [-999,-999]
-        i.append(dismod)
-        i.append(kpc_per_arcsec)
-        cursor.execute(cmd +' (%f, %f, %f, %d, %f, %f, %d, %f, %f, %f, %f, %f, %d);' %tuple(i))
-    
+        cmd+=' (%f, %f, %f, %d, %f, %f, %d, %f, %f, %f, %f, %f, %d, %f, %f) ' %(i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7],i[8],i[9],i[10],i[11],patch, dismod, kpc_per_arcsec)
+        count+=1
+        if count % 1000==0:
+            print count
+            cursor.execute(cmd+';')
+            cmd = "insert into %s (ra_mid, dec_mid, z, id1, ra1, dec1, id2, ra2, dec2, R_pair, drlos, dz, patch, dismod, kpc_per_arcsec) values " %tablename
+        else:
+            cmd+=','
     hdu.close()
     cursor.Conn.commit()
 
